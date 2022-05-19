44c20db3b1eea4efce31d2800f0bdc6d9cc5fbff
Alex Folgin <folgin@gmail.com>
2022-05-19 12:15:33 +0100

    Initial Commit

---
+++ AppCoordinatorTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppCoordinatorTest
+//
+//  Created by Alex Folgin on 19.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
